a7f231c995c61c22c4f2de7fb58ef63d99e9e134
neogeomat <amrit.im@gmail.com>
2019-05-22 23:56:47 +0200

    fixed error in node order in output

---
+++ amrit.py
@@ -0,0 +1,34 @@
+import json, time
+from dijkstra import Graph, createGraphFromGeojson
+
+config = {}
+config['graph_file'] = 'graph.json'
+config['nodes_file'] = 'selected_nodes.geojson'
+config['edges_file'] = 'selected_edges.geojson'
+config['landmarks_file'] = 'selected_landmarks.geojson'
+
+try:
+    with open(config['graph_file'],'r') as fp:
+        edges = json.load(fp)
+except:
+    print("graph file doesn't exist ")
+    edges = createGraphFromGeojson(config['nodes_file'],config['edges_file'])
+try:
+    with open(config['landmarks_file'],'r') as fp:
+        landmarks_json = json.load(fp)
+        landmarks = landmarks_json["features"]
+except:
+    landmarks = {}
+if(edges):
+    graph = Graph(edges,landmarks,config)
+else:
+    raise
+
+
+print(graph.dijkstra("1101", "1098").result)
+t4 = time.time()
+graph.get_geom_from_path()
+print(graph.result['geom_path'])
+with open('simple_djikstra.geojson','w') as f:
+    f.write(graph.to_geojson())
+    f.close()
--- dijkstra.py
+++ dijkstra.py
@@ -7,23 +7,18 @@ import pickle
 inf = float('inf')
 Edge = namedtuple('Edge', 'start, end, cost')
 
-config = {}
-config['graph_file'] = 'graph.json'
-config['nodes_file'] = 'selected_nodes.geojson'
-config['edges_file'] = 'selected_edges.geojson'
-
 print("Program Started at " + str(time.time()))
 
 def make_edge(start, end, cost=1):
     return Edge(start, end, cost)
 
-def createGraphFromGeojson(node_file=config['nodes_file'], edge_file=config['edges_file']):
+def createGraphFromGeojson(node_file="selected_nodes.geojson", edge_file='selected_nodes.geojson'):
         t1 = time.time()
-        with open(config['edges_file'], "r") as read_file:
+        with open(edge_file, "r") as read_file:
             edges_json = json.load(read_file)
             edges = edges_json["features"]
 
-        with open(config['nodes_file'], "r") as read_file:
+        with open(node_file, "r") as read_file:
             nodes_json = json.load(read_file)
             nodes = nodes_json["features"]
         
@@ -53,22 +48,17 @@ def createGraphFromGeojson(node_file=config['nodes_file'], edge_file=config['edg
 
 class Graph:
     # graph.pickle is the saved graph, if it doesn't exist new one is created from the geojson files.
-    def __init__(self, edges=[]):
+    def __init__(self, edges=[], landmarks={},config={}):
         # let's check that the data is right
         wrong_edges = [i for i in edges if len(i) not in [2, 3]]
         if wrong_edges:
             raise ValueError('Wrong edges data: {}'.format(wrong_edges))
 
-        self.edges = [make_edge(*edge) for edge in edges]
+        self.edges = edges = [Edge(*edge) for edge in edges]
+        self.vertices = {e.start for e in edges} | {e.end for e in edges}
+        self.landmarks = landmarks
+        self.config = config
         self.result = {}
-
-    @property
-    def vertices(self):
-        return set(
-            sum(
-                ([edge.start, edge.end] for edge in self.edges), []
-            )
-        )
 
     def get_node_pairs(self, n1, n2, both_ends=True):
         if both_ends:
@@ -94,15 +84,44 @@ class Graph:
         if both_ends:
             self.edges.append(Edge(start=n2, end=n1, cost=cost))
 
-    @property
-    def neighbours(self):
-        neighbours = {vertex: set() for vertex in self.vertices}
-        for edge in self.edges:
-            neighbours[edge.start].add((edge.end, edge.cost))
-
-        return neighbours
-
     def dijkstra(self, source, dest):
+        assert source in self.vertices, 'Such source node doesn\'t exist'
+        distances = {vertex: inf for vertex in self.vertices}
+        previous_vertices = {
+            vertex: None for vertex in self.vertices
+        }
+        distances[source] = 0
+        vertices = self.vertices.copy()
+        neighbours = {vertex: set() for vertex in self.vertices}
+        removedvertices = []
+
+        for start, end, cost in self.edges:
+            neighbours[start].add((end, cost))
+        while vertices:
+            current_vertex = min(
+                vertices, key=lambda vertex: distances[vertex])         
+            
+            for neighbour, cost in neighbours[current_vertex]:
+                alternative_route = distances[current_vertex] + cost
+                if alternative_route < distances[neighbour]:
+                    distances[neighbour] = alternative_route
+                    previous_vertices[neighbour] = current_vertex
+            if distances[current_vertex] == inf or current_vertex == dest:
+                break
+            vertices.remove(current_vertex)
+            removedvertices.append(current_vertex)
+
+        path, current_vertex = deque(), dest
+        while previous_vertices[current_vertex] is not None:
+            path.appendleft(current_vertex)
+            current_vertex = previous_vertices[current_vertex]
+        if path:
+            path.appendleft(current_vertex)
+        self.result["path"] = path
+        self.result["removedvertices"] = removedvertices
+        return self
+    
+    def dijkstra_landmark(self, source, dest):
         assert source in self.vertices, 'Such source node doesn\'t exist'
         distances = {vertex: inf for vertex in self.vertices}
         previous_vertices = {
@@ -117,6 +136,7 @@ class Graph:
                 vertices, key=lambda vertex: distances[vertex])         
             
             for neighbour, cost in self.neighbours[current_vertex]:
+                # do sth for land marks here
                 alternative_route = distances[current_vertex] + cost
                 if alternative_route < distances[neighbour]:
                     distances[neighbour] = alternative_route
@@ -137,42 +157,15 @@ class Graph:
         return self
 
     def get_geom_from_path(self):
-        with open(config['nodes_file'], "r") as read_file:
+        with open(self.config['nodes_file'], "r") as read_file:
             nodes_json = json.load(read_file)
             nodes = nodes_json["features"]
         self.result['geom_path'] = []
-        for node in nodes:
-            for vertex in self.result["path"]:
+        for vertex in self.result["path"]:
+            for node in nodes:
                 if(node["properties"]["nodeID"] == int(vertex)):
-                    self.result['geom_path'].append([node["geometry"]["coordinates"]])
+                    self.result['geom_path'].append(node["geometry"]["coordinates"])
         return self
-
-try:
-    with open(config['graph_file'],'r') as fp:
-        edges = json.load(fp)
-except:
-    print("graph file doesn't exist ")
-    edges = createGraphFromGeojson()
-graph = Graph(edges)
-
-
-# example of the Graph structure
-# graph = Graph([
-#     ("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
-#     ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
-#     ("e", "f", 9)])
-t3 = time.time()
-# print("time to create graph",t3-t2)
-# find a route
-print(graph.dijkstra("1101", "1098").result)
-t4 = time.time()
-print("time to find route",t4-t3)
-graph.get_geom_from_path()
-print(graph.result['geom_path'])
-# print(graph.dijkstra("5907", "460"))
-# t4 = time.time()
-# print("time to finf route", t4-t3)
-
-# TODO find the respective coordinates (nodes, edges? what should be the
-# return format?
-# TODO start and destination points are probably not exactly the nodes
+    
+    def to_geojson(self):
+        return '{"type": "FeatureCollection","crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::25832" } },"features": [{"type": "Feature","properties":{"e":"m"},"geometry": {"type": "LineString","coordinates":'+str(self.result['geom_path'])+'}}]}' 
